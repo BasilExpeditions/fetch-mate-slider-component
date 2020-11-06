5ea3d1d6f7a09202482027c69c659ab89c911bfe
Bastien Durand <basilexpeditions@gmail.com>
2020-11-06 12:59:59 +1100

    modified component for pets

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css'
+
+import SwipeCard from './SwipeCard'
+
+
+function App() {
+  return (
+    <div className="app">
+      <SwipeCard />
+    </div>
+  );
+}
+
+export default App;
